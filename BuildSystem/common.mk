375ef5a9f4a85b0ca6bd6bde8c5dd3b297ecd9f1
Steven W. Klassen <klassens@acm.org>
2018-10-13 21:47:13 -0500

    Made the build system supportive of a "configure" script
    Closes #5

--- BuildSystem/common.mk
+++ BuildSystem/common.mk
@@ -7,79 +7,31 @@ PACKAGEBASENAME := $(shell echo $(PACKAGEBASENAME) | sed 's/^$(PREFIX)//')
 BUILDSYSTEMDIR := $(PROJECTDIR)/BuildSystem
 VERSION := $(shell $(BUILDSYSTEMDIR)/revision.sh)
 VERSIONMACRO := $(shell echo $(PREFIX)$(PACKAGEBASENAME) | tr '[:lower:]' '[:upper:]')_VERSION
+PREFIX := /usr/local
 
 OS := $(shell uname -s)
 ARCH := $(OS)-$(shell uname -m)
 BUILDDIR := .build/$(ARCH)
 HEADERDIR := $(BUILDDIR)/include/$(PREFIX)/$(PACKAGEBASENAME)
 LIBDIR := $(BUILDDIR)/lib
-APPDIR := $(BUILDDIR)/app
 
 ifeq ($(OS),Darwin)
 	SOEXT := .$(VERSION).dylib
 	SOEXT_SHORT := .dylib
 	LDPATHEXPR := DYLD_LIBRARY_PATH="$(LIBDIR)"
 	CC := clang
-	C++ := clang++
+	CXX := clang++
 else
 	SOEXT := .so.$(VERSION)
 	SOEXT_SHORT := .so
 	LIBS := $(LIBS) -lpthread
 	LDPATHEXPR := LD_LIBRARY_PATH="$(LIBDIR)"
 	CC := gcc
-	C++ := g++
+	CXX := g++
 	CFLAGS := $(CFLAGS) -fPIC 
 endif
 
-.phony: build library application check clean cleanall directory-checks hello prep
-
-LIBNAME := $(PREFIX)$(PACKAGEBASENAME)
-LIBFILE := lib$(LIBNAME)$(SOEXT)
-LIBFILELINK := lib$(LIBNAME)$(SOEXT_SHORT)
-LIBPATH := $(LIBDIR)/$(LIBFILE)
-
-EXENAME := $(PACKAGEBASENAME)
-EXEPATH := $(APPDIR)/$(EXENAME)
-
-TESTDIR := $(BUILDDIR)/tests
-TESTPATH := $(TESTDIR)/unittest
-
-
-# Turn off the building of the library, application, and tests, based on the existance
-# of Sources/main.cpp and Tests directories.
-
-ifeq ($(wildcard Sources/main.cpp),)
-	EXEPATH :=
-else
-	LIBPATH :=
-endif
-
-ifeq ($(wildcard Tests/.*),)
-	TESTPATH :=
-endif
-
-build: library application
-
-library: $(LIBPATH)
-
-application: $(EXEPATH)
-
-
-# Use "make hello" to test that the names are what you would expect.
-
-hello:
-	@echo "Hello from $(PACKAGEBASENAME)"
-	@echo "  PROJECTDIR=$(PROJECTDIR)"
-	@echo "  VERSION=$(VERSION)"
-	@echo "  HEADERDIR=$(HEADERDIR)"
-	@echo "  LIBNAME=$(LIBNAME)"
-	@echo "  LIBPATH=$(LIBPATH)"
-	@echo "  EXEPATH=$(EXEPATH)"
-ifneq ($(wildcard Tests/.*),)
-	@echo "  TESTPATH=$(TESTPATH)"
-endif
-
-CFLAGS := $(CFLAGS) -Wall -I$(BUILDDIR)/include
+CFLAGS := $(CFLAGS) -Wall
 OPTIMIZED_FLAGS := -O2 -DNDEBUG
 DEBUGGING_FLAGS := -g
 ifeq ($(DEBUG),true)
@@ -88,8 +40,53 @@ else
 	CFLAGS := $(CFLAGS) $(OPTIMIZED_FLAGS)
 endif
 
-CPPFLAGS := $(CPPFLAGS) $(CFLAGS) -std=c++14 -Wno-unknown-pragmas
+CXXFLAGS := $(CXXFLAGS) $(CFLAGS)
 LDFLAGS := $(LDFLAGS) -L$(LIBDIR)
+
+-include $(PROJECTDIR)/config.local
+-include $(PROJECTDIR)/config.defs
+
+CFLAGS := $(CFLAGS) -I$(BUILDDIR)/include
+CXXFLAGS := $(CXXFLAGS) -I$(BUILDDIR)/include -std=c++14 -Wno-unknown-pragmas
+
+.phony: build library install check clean cleanall directory-checks hello prep
+
+LIBNAME := $(PREFIX)$(PACKAGEBASENAME)
+LIBFILE := lib$(LIBNAME)$(SOEXT)
+LIBFILELINK := lib$(LIBNAME)$(SOEXT_SHORT)
+LIBPATH := $(LIBDIR)/$(LIBFILE)
+
+TESTDIR := $(BUILDDIR)/tests
+TESTPATH := $(TESTDIR)/unittest
+
+
+# Turn off the building of the tests if there is no Tests directory.
+ifeq ($(wildcard Tests/.*),)
+	TESTPATH :=
+endif
+
+build: library
+
+library: $(LIBPATH)
+
+
+# Use "make hello" to test that the names are what you would expect.
+
+hello:
+	@echo "Hello from $(PACKAGEBASENAME)"
+	@echo "  PROJECTDIR=$(PROJECTDIR)"
+	@echo "  PREFIX=$(PREFIX)"
+	@echo "  VERSION=$(VERSION)"
+	@echo "  HEADERDIR=$(HEADERDIR)"
+	@echo "  LIBNAME=$(LIBNAME)"
+	@echo "  LIBPATH=$(LIBPATH)"
+	@echo "  CC=$(CC)"
+	@echo "  CFLAGS=$(CFLAGS)"
+	@echo "  CXX=$(CXX)"
+	@echo "  CXXFLAGS=$(CXXFLAGS)"
+ifneq ($(wildcard Tests/.*),)
+	@echo "  TESTPATH=$(TESTPATH)"
+endif
 
 # Build the library
 
@@ -107,11 +104,11 @@ PREPS := Sources/_version_internal.h Sources/all.h $(HEADERDIR)
 prep: $(PREPS)
 
 $(LIBPATH): $(PREPS) $(LIBDIR) $(OBJS)
-	$(C++) $(LDFLAGS) -shared $(OBJS) $(LIBS) -o $@
+	$(CXX) $(LDFLAGS) -shared $(OBJS) $(LIBS) -o $@
 	(cd $(LIBDIR) ; rm -f $(LIBFILELINK) ; ln -s $(LIBFILE) $(LIBFILELINK))
 
 $(BUILDDIR)/%.o: Sources/%.cpp $(HDRS)
-	$(C++) -c $< $(CPPFLAGS) -o $@
+	$(CXX) -c $< $(CXXFLAGS) -o $@
 
 $(BUILDDIR)/%.o: Sources/%.c $(HDRS)
 	$(CC) -c $< $(CFLAGS) -o $@
@@ -144,14 +141,6 @@ $(HEADERDIR):
 	-ln -s `pwd`/Sources $(BUILDDIR)/include/$(PREFIX)/$(PACKAGEBASENAME)
 
 
-# Build the executable.
-
-$(EXEPATH): $(PREPS) $(EXEDIR) $(OBJS) $(BUILDDIR)/main.o
-	$(C++) $(LDFLAGS) $(OBJS) $(BUILDDIR)/main.o $(LIBS) -o $@
-
-$(EXEDIR):
-	-mkdir -p $@
-
 # Build and run the unit tests.
 
 TESTSRCS := $(wildcard Tests/*.cpp)
@@ -162,26 +151,28 @@ TESTSRCS := $(filter-out Sources/main.cpp, $(TESTSRCS))
 TESTOBJS := $(patsubst Tests/%.cpp,$(TESTDIR)/%.o,$(TESTSRCS))
 TESTHDRS := $(wildcard Tests/*.h)
 
-check: library application $(TESTPATH)
+check: library $(TESTPATH)
 ifneq ($(wildcard $(EXEPATH)),)
 	$(LDPATHEXPR) $(EXEPATH) --version
 endif
 	$(LDPATHEXPR) $(TESTPATH)
 
 $(TESTPATH): $(LIBPATH) $(TESTDIR) $(TESTOBJS)
-	$(C++) $(LDFLAGS) -L$(BUILDDIR) $(TESTOBJS) -l $(LIBNAME) $(LIBS) -o $@
+	$(CXX) $(LDFLAGS) -L$(BUILDDIR) $(TESTOBJS) -l $(LIBNAME) $(LIBS) -o $@
 
 $(TESTDIR):
 	-mkdir -p $@
 
 $(TESTDIR)/%.o: Tests/%.cpp $(TESTHDRS)
-	$(C++) -c $< $(CPPFLAGS) -I. -o $@
+	$(CXX) -c $< $(CXXFLAGS) -I. -o $@
 
 
+# Perform the install.
+install:
 
 # Clean the build.
 clean:
-	rm -rf $(BUILDDIR) REVISION Sources/_version_internal.h Sources/all.h Tests/unit_test_main.cpp
+	rm -rf $(BUILDDIR) REVISION Sources/_version_internal.h Sources/all.h
 
 cleanall: clean
-	rm -rf .build
+	rm -rf .build config.defs config.target.defs
